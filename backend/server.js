cc7e7a4185b431d0fedea0742d218caebd5e1537
adrijanasavic <adrijana.jovicic@gmail.com>
2023-01-24 13:07:14 +0100

    get data from server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+let lolChampions = require("lol-champions");
+// console.log(lolChampions);
 
 const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(lolChampions);
+});
 
 app.listen(4000, () => {
     console.log('Listening on post 4000');
